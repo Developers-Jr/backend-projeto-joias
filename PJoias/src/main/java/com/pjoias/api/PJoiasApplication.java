4bc01b2ff4ff040ef06b104e5cf11dc55bee6289
caioom <caio.ds.2003@gmail.com>
2020-05-08 17:05:37 -0300

    Starter do projeto criado, adição de esquema para funcionalidades

---
+++ PJoias/src/main/java/com/pjoias/api/PJoiasApplication.java
@@ -0,0 +1,13 @@
+package com.pjoias.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PJoiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PJoiasApplication.class, args);
+	}
+
+}
---
+++ PJoias/src/test/java/com/pjoias/api/PJoiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjoias.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PJoiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
