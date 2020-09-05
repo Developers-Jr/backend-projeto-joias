193614bf1d6f9bd3461464192140947f21cabc13
caioom <caio.ds.2003@gmail.com>
2020-09-04 21:59:17 -0300

    Feature: Swagger-ui

--- src/main/java/com/caio/pjoias/controllers/VendedorController.java
+++ src/main/java/com/caio/pjoias/controllers/VendedorController.java
@@ -5,11 +5,15 @@ import com.caio.pjoias.dtos.out.VendedorDtoOut;
 import com.caio.pjoias.exceptions.VendedorException;
 import com.caio.pjoias.models.Vendedor;
 import com.caio.pjoias.services.VendedorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -23,12 +27,17 @@ public class VendedorController {
         this.vendedorService = vendedorService;
     }
 
+    @Operation(summary = "Cadastrar vendedor", responses = {
+            @ApiResponse(description = "Sucesso", responseCode = "201"),
+            @ApiResponse(description = "Falha", responseCode = "400")})
     @PostMapping
-    public HttpStatus cadastrarNovoVendedor(@Valid @RequestBody VendedorDtoIn vendedorDtoIn) throws VendedorException {
+    public ResponseEntity<Void> cadastrarNovoVendedor(@Valid @RequestBody VendedorDtoIn vendedorDtoIn) throws VendedorException {
         this.vendedorService.persistirNovo(new Vendedor(vendedorDtoIn));
-        return HttpStatus.CREATED;
+        return ResponseEntity.created(URI.create("/v1/vendedor")).build();
     }
 
+    @Operation(description = "Listar vendedores", responses = {
+            @ApiResponse(description = "Sucesso", responseCode = "200")})
     @GetMapping
     public ResponseEntity<List<VendedorDtoOut>> retornarTodos() {
         var vendedores = this.vendedorService.retornarTodos()
